5b037b549f6a2d568b96fecd830d11dd0702dd59
Moto42 <moto42@gmail.com>
2018-11-18 17:17:51 -0600

    Now using Tabber to display app components.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 // import Header     from './components/Header';
-// import BookAdder  from './components/BookAdder';
-// import StackAdder from './components/StackAdder';
+import Tabber     from './components/Tabber';
+import BookAdder  from './components/BookAdder';
+import StackAdder from './components/StackAdder';
 import BookSearch from './components/BookSearch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BookSearch />
+        <Tabber>
+          <BookSearch tabName = 'BookSearch' />
+          <StackAdder tabName = 'StackAdder' />
+          <BookAdder  tabName = 'BookAdder' />
+        </Tabber>
       </div>
     );
   }
 }
 
 export default App;
-
-        // <StackAdder />
-        // <BookAdder />
