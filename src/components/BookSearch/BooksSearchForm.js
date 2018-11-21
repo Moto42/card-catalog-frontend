f44ef0878b2ca5b4a85dfc08e7603a61a9f78f4f
Moto42 <moto42@gmail.com>
2018-11-21 01:43:38 -0600

    Checklists filtering is in.! Checklists are filtered to only show choices relavent to the currently displayed results.

--- src/components/BookSearch/BooksSearchForm.js
+++ src/components/BookSearch/BooksSearchForm.js
@@ -3,15 +3,26 @@ import ChecklistDisplay     from './ChecklistDisplay';
 
 class BooksSearchForm extends Component {
 
+  filterByGenre(books, checklist){
+    let returnList = checklist.filter( c => books.some( b => b.genre.some( g => g === c[0] ) ) )
+    return returnList;
+  }
+
+  filterBySubject(books, checklist){
+    let returnList = checklist.filter( c => books.some( b => b.subject.some( g => g === c[0] ) ) )
+    return returnList;
+  }
+
   render() {
+    const { genreList, subjectsList, booksList } = this.props;
     return (<div id='BooksSearchForm'>
       <input type='text' placeholder = 'Title Search' onChange={this.props.updateSearchString}/>
       <hr />
       <em>Genre Filters</em>
-      <ChecklistDisplay list={this.props.genreList} callback={this.props.genreTicker} />
+      <ChecklistDisplay list={this.filterByGenre(booksList, genreList)} callback={this.props.genreTicker} />
       <hr />
       <em>Subjects Filter</em>
-      <ChecklistDisplay list={this.props.subjectsList} callback={this.props.subjectTicker} />
+      <ChecklistDisplay list={this.filterBySubject(booksList, subjectsList)} callback={this.props.subjectTicker} />
     </div>)
   }
 }
