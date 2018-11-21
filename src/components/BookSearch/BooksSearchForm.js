7aec5ad659858bec1acddbd3161eab84b459f786
Moto42 <moto42@gmail.com>
2018-11-20 18:14:12 -0600

    Seperated and labeld genre and subject filters

--- src/components/BookSearch/BooksSearchForm.js
+++ src/components/BookSearch/BooksSearchForm.js
@@ -5,7 +5,11 @@ class BooksSearchForm extends Component {
   render() {
     return (<div id='BooksSearchForm'>
       <input type='text' placeholder = 'Title Search' onChange={this.props.updateSearchString}/>
+      <hr />
+      <em>Genre Filters</em>
       <ChecklistDisplay list={this.props.genreList} callback={this.props.genreTicker} />
+      <hr />
+      <em>Subjects Filter</em>
       <ChecklistDisplay list={this.props.subjectsList} callback={this.props.subjectTicker} />
     </div>)
   }
